db58f815d38415d9a831da876f5be03e0b09238d
varant-t <vv_titizian@hotmail.ca>
2021-04-25 19:57:02 -0400

    Pushie Wushie

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    // Start is called before the first frame update
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
