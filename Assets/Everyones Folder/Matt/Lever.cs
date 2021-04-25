7f1f3d2b189fbb2b0ad7b49fb916d63b11766126
husky339 <bendermaj@gmail.com>
2021-04-24 23:00:38 -0400

    Animation putting pedestal fix

---
+++ Assets/Everyones Folder/Matt/Lever.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseOver()
+    {
+        
+    }
+}
