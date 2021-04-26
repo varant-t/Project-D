207655640f06fa3c390d585eb69a6bf102f9fb18
varant-t <vv_titizian@hotmail.ca>
2021-04-25 20:07:25 -0400

    Revert "Revert "Pushie Wushie""
    This reverts commit b6fc520f7d60b205a062aa236562a56a88115b82.

---
+++ Assets/mouseCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCursor : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+   
+    private void Start()
+    {
+        //Cursor.visible = false;
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
